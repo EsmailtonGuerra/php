e9aa2f8cc7d60d7117150366ab447a6e8891a5ea
Esmailton G. F.tas <75494686+EsmailtonGuerra@users.noreply.github.com>
2025-05-26 10:46:26 -0300

    Exercício 22
     Faça um código que informe os nome do Instrutor, Rafael e  5 números, multiplique os 5 números.

---
+++ ex_22_multiplique.php
@@ -0,0 +1,22 @@
+<?php
+// nome do instrutor
+$instrutor = "Rafael";
+
+// numeros para multiplicar
+$num1 = 2;
+$num2 = 3;
+$num3 = 4;
+$num4 = 5;
+$num5 = 6;
+
+// Multiplicacao dos 5  numeros
+$resultado = $num1 * $num2 * $num3 * $num4 * $num5;
+
+
+// Eibindo as informacoes
+echo "Nome do instrutor: " . $instrutor . "\n";
+echo "Numeros: $num1, $num2, $num3, $num4, $num5\n";
+echo "resultado da multiplicacao: " . $resultado;
+?>
+
+
