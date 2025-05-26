18b663fd7054a5b44602fe3ef985288e98c4ca27
Esmailton G. F.tas <75494686+EsmailtonGuerra@users.noreply.github.com>
2025-05-26 13:57:21 -0300

    Exercício 16
     -Exiba uma mensagem de apresentação Concatenação

---
+++ concatenacao.php
@@ -0,0 +1,9 @@
+<?php
+// Variáveis de apresentação
+$nome = "Esmailtom";
+$idade = 38;
+$cidade = "Brasilia";
+
+// Mensagem com concatenação
+echo "Olá! Meu nome é " . $nome . ", tenho " . $idade . " anos e moro em " . $cidade . ".";
+?>
