6f59f1b2d134a8f0b21edd2f2371d5298dfc8148
Esmailton G. F.tas <75494686+EsmailtonGuerra@users.noreply.github.com>
2025-05-29 09:12:18 -0300

    Exercício 25
    Faça um código PHP que utiliza if e else para verificar se uma pessoa pode votar

---
+++ exe25if_else_votar.php
@@ -0,0 +1,12 @@
+<?php
+// definindob a idade da pessoa 
+
+echo "idade informada: $idade anos<br>";
+
+// verifica se a idade é maior ou igual a 16
+if ($idade >= 16) {
+    echo "VOCE PODE VOTAR!";
+}else {
+    echo "voce ainda nao pode votar.";
+}
+?>
