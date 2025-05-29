2c04da5aba66cbb1ab40db776d9ebae361dbe080
Esmailton G. F.tas <75494686+EsmailtonGuerra@users.noreply.github.com>
2025-05-29 09:49:58 -0300

    Exercício 26
    Faça um código PHP que utiliza if e else para verificar se um número é positivo, negativo ou zero

---
+++ Ex_26if_else_negativo.php
@@ -0,0 +1,16 @@
+<?php
+//definindo o numero
+$numero = 0;
+
+echo "numero informado: $numero<br>";
+
+// verificaçacao se o numero e positivo, negativo ou zero
+if ($numero > 0) {
+    echo "onumero e positivo.";   
+}elseif ($numero < 0){
+   echo "o numero e negativo.";
+}else {
+   echo "o numero e zero.";
+}
+?>
+
