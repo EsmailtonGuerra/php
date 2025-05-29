54791a360e2f0f81446c7ad81845f558b3192725
Esmailton G. F.tas <75494686+EsmailtonGuerra@users.noreply.github.com>
2025-05-29 10:47:26 -0300

    Exercício 27
    –Faça um código PHP que utiliza if e else para verificar se um número é par ou ímpar:

---
+++ exe27if_else_par_impar.php
@@ -0,0 +1,15 @@
+<?php
+//definindo o numero 
+$numero = 7;
+
+echo "numero informado: $numero<br>";
+
+// verifica se o numero e impar ou impar
+if ($numero % 2 == 0) {
+    echo "o numero e par.";
+
+}else{
+    echo "o numero e impar.";
+
+}
+?>
